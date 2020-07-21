5b9ec713a431e8328e87f263894ce7968a599c8a
Shawn Kyzer <shawnkyzer@gmail.com>
2020-07-21 15:26:14 +0200

    Setup unit test to convert to proto for payments, update the readme and roll in mockito

--- README.md
+++ README.md
@@ -14,3 +14,6 @@ _Some notes or explaination of your solution..._
 
 _If u have more time or want to improve somthing..._
 1. I would change the producer to use protobuf directly so that the deserialization will be easier and also more compact as a byte[]
+2. Would add in SonarQube, Jacoco, Snyke and google checkstyle as part of the final build CI/CD process
+3. More unit testing for the kafka stream and better integration tests
+3.
--- src/test/java/com/technicaltest/payment/service/PurchaseOrderEventConsumerTest.java
+++ src/test/java/com/technicaltest/payment/service/PurchaseOrderEventConsumerTest.java
@@ -1,10 +1,13 @@
 package com.technicaltest.payment.service;
 
+import com.technicaltest.payment.service.proto.Payments.Payment;
+import org.apache.kafka.clients.consumer.Consumer;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 public class PurchaseOrderEventConsumerTest {
 
@@ -26,6 +29,29 @@ public class PurchaseOrderEventConsumerTest {
 
     @Test
     public void convertToPayment() {
+        Consumer<String,String> mockedConsumer = mock(Consumer.class);
+        boolean isKafkaAvailable = true;
+        PurchaseOrderEventConsumer underTest =
+                new PurchaseOrderEventConsumer(isKafkaAvailable, mockedConsumer);
 
+        String messageValue = "{\"payment_id\": \"20680a5d-2f0e-4d8d-9910-bd8a455c2df7\", " +
+                "\"account_id\": 468, " +
+                "\"payment_type\":\"offline\", " +
+                "\"credit_card\": \"\", " +
+                "\"amount\": 32, " +
+                "\"delay\": 221}";
+
+        Payment expected = Payment.newBuilder()
+                .setPaymentId("20680a5d-2f0e-4d8d-9910-bd8a455c2df7")
+                .setAccountId(468)
+                .setCreditCard("")
+                .setPaymentType("offline")
+                .setAmount(32)
+                .setDelay(221)
+                .build();
+
+        Payment actual = underTest.convertToPayment(messageValue);
+
+        assertEquals(expected,actual);
     }
 }
